970eb30ddcca9965efe9aa8efca1015ae80199d3
brians97 <isanchez.brian@gmail.com>
2023-02-20 15:45:48 -0800

    added create and delete item functionality

--- app.js
+++ app.js
@@ -19,6 +19,51 @@ function displayDate() {
     document.getElementById('date').innerHTML = date
 }
 
+function displayItems() {
+    let items = ''
+    for (let i = 0; i < itemsArray.length; i++){
+        items += 
+        `<div class='item'>
+            <div class="input-controller">
+                <textarea disabled>${itemsArray[i]}</textarea>
+                <div class="edit-controller">
+                    <i class="fa-solid fa-trash-can deleteBtn"></i>
+                    <i class="fa-solid fa-pen-to-square editBtn"></i>
+                </div>
+            </div>
+            <div class="update-controller">
+                <button class="saveBtn">Save</button>
+                <button class="cancelBtn">Cancel</button>
+            </div>
+        </div>`
+    }
+    document.getElementById('todos').innerHTML = items
+    activateDeleteListeners();
+}
+
+function activateDeleteListeners() {
+    let deleteBtn = document.querySelectorAll('.deleteBtn')
+    deleteBtn.forEach((dB, i) => {
+        dB.addEventListener('click', () => {
+            deleteItem(i)
+        })
+    })
+}
+
+function createItem(item){
+    itemsArray.push(item.value)
+    localStorage.setItem('items', JSON.stringify(itemsArray))
+    location.reload();
+}
+
+function deleteItem(i){
+    itemsArray.splice(i, 1)
+    localStorage.setItem('items', JSON.stringify(itemsArray))
+    location.reload()
+}
+
+
 window.onload = function() {
-    displayDate()
+    displayDate();
+    displayItems();
 }
